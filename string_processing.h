03c3816a33701297fae98dff94526c3717c026dd
exceedololo <77617952+exceedololo@users.noreply.github.com>
2022-03-09 19:14:20 +0300

    Delete string_processing.h

--- string_processing.h
+++
@@ -1,7 +0,0 @@
-// в качестве заготовки кода используйте последнюю версию своей поисковой системы
-#pragma once
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> SplitIntoWords(const std::string&);
