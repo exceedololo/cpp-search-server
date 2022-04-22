586bb8d63358142c27890e219aa9f4ab6e82fae1
exceedololo <77617952+exceedololo@users.noreply.github.com>
2022-04-23 00:41:46 +0300

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -1,26 +1,6 @@
 // в качестве заготовки кода используйте последнюю версию своей поисковой системы
 #include "string_processing.h"
 
-std::vector<std::string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        } else {
-            word += c;
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-
-    return words;
-}
-
 std::vector<std::string_view> SplitIntoWords(std::string_view str) {
     std::vector<std::string_view> result;
     while (true) {
